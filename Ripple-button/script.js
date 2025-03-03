f84815ffd5ae012f3ffd95c877c6f4546d05d46c
Musama8014 <muhammadusama8014@gmail.com>
2025-03-03 10:28:22 +0000

    Added new project

---
+++ Ripple-button/script.js
@@ -0,0 +1,35 @@
+const buttons = document.querySelectorAll('.ripplebtn')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.pageX
+        const y = e.pageY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+
+        setTimeout(() => circle.remove(), 500)
+    })
+})
+buttons.forEach(button =>
+{
+    button.addEventListener('click',function()
+    {
+    this.classList.add('zoom-out');
+setTimeout(
+    ()=>{
+        this.classList.remove('zoom-out');
+    }, 300);
+});
+});
+
